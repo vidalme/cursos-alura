b871b3f4e26794b9ea2b5ccb09a36d39481b772a
andre <andrinho@gmail.com>
2024-03-22 04:29:40 -0300

    lots of progress on log backup script

--- __PROJETOS_PRATICOS/01.automacao_python/backup_logs/backup_server_logs.py
+++ __PROJETOS_PRATICOS/01.automacao_python/backup_logs/backup_server_logs.py
@@ -41,52 +41,108 @@ def usage_message():
 
 def time_stamp():
     t = datetime.now()
-    time_label = f"{t.year}-{t.month}-{t.day}_{t.hour}-{t.minute}-{t.second}-{t.microsecond}_"
+    ano = t.strftime("%Y")
+    mes = t.strftime("%m")
+    dia = t.strftime("%d")
+    hora = t.strftime("%H")
+    minuto = t.strftime("%M")
+    segundo = t.strftime("%S")
+    time_label = f"{ano}{mes}{dia}_{hora}-{minuto}---{segundo}_"
+    # time_label = f"{ano}-{mes}-{dia}"
     return time_label
 
 def main():
     
-    # encontra_ip = "grep -E '^\\s{4}inet\\s.+enp0s3$'"
-    # encontra_successful_logins = "grep -iE 'sshd:session' | grep -vi 'failed'"
-    # encontra_successful_logins = "grep -iE 'sshd'"
-    # encontra_failed_logins = "grep -iE 'sshd.+failed'"
-
     try:
-        pass
 
         with open("config.yaml","r") as f:
+
+            #carrega as configuracoes do serviço
             config = yaml.safe_load(f)
+            
+            #destino de todos os backups é definido no arquivo config.yaml e pode ser alterado pra qualquer lugar
+            destiny = config['destiny']
+            #numero maximo de arquivos acumulados de backup
+            max_acumulado = config['period']
+            #cria a pasta destino caso ainda nao exista
+            if not os.path.exists(destiny): subprocess.run(['mkdir',f'{destiny}'])
 
+            #variavel com os nomes dos diretorios separados pelos bem sucedidos e os que falharam
+            sl_fol = "successful_logins"
+            fl_fol = "failed_logins"
 
-
+        #loop na lista de servidores a fazerem backup dos seus logs
         for s in config['servers']:
+            #usuario que logara no sistema
             u = s['user']
+            #ip do servidor
             ip = s['server_ip']
+            #hosname
             name = s['name']
 
+            #conecta simplao
             c = Connection(host=f"{u}@{ip}")
+            
+            #cria o um path especifico para esse servidor do loop
+            destiny_plus = os.path.join(destiny,name)
 
-            # print(f"{time_stamp()}successful_logins.txt")
+            #cria paths para os diretorios dentro do diretorio do servidor
+            sl_dir = os.path.join(destiny_plus,sl_fol)    
+            fl_dir = os.path.join(destiny_plus,fl_fol)    
 
-            successful_logins = c.run(f"journalctl -S today | grep -iE 'sshd:session'")
-            failed_logins = c.run(f"journalctl -S today | grep -iE 'sshd.+failed'")
+            #cria diretorio com nome do servidor
+            if not os.path.exists(destiny_plus):subprocess.run(['mkdir',f'{destiny_plus}'])
+            #cria diretorio para os logins de sucesso
+            if not os.path.exists(sl_dir):subprocess.run(['mkdir',f'{sl_dir}'])
+            #cria diretorio para os logins que falharam
+            if not os.path.exists(fl_dir):subprocess.run(['mkdir',f'{fl_dir}'])
 
-            with open (f"{name}_{time_stamp()}successful_logins.txt","w") as sl:
-                sl.write(successful_logins.stdout)
+            #cria paths pros arquivos de backuip, logins de sucesso e falhos
+            sl_filename = os.path.join(sl_dir,f"{time_stamp()}.txt")
+            fl_filename = os.path.join(fl_dir,f"{time_stamp()}.txt")
 
-            with open (f"{name}_{time_stamp()}failed_logins.txt","w") as fl:
-                fl.write(failed_logins.stdout)
+            if len(os.listdir(sl_dir)) == max_acumulado:
+                all_sl_files = [ os.path.join(sl_dir,f) for f in os.listdir(sl_dir) ] 
+                all_sl_files.sort()
+                os.remove(all_sl_files[0])
+                print('agora estou com o maximo numero de arquivios! Apague o arquivo mais antigo.')
 
-            # print(successful_logins)
-            # print(failed_logins)
-            print('--------------')
+            if len(os.listdir(fl_dir)) == max_acumulado:
+                all_fl_files = [ os.path.join(fl_dir,f) for f in os.listdir(fl_dir) ] 
+                all_fl_files.sort()
+                os.remove(all_fl_files[0])
+                print('agora estou com o maximo numero de arquivios! Apague o arquivo mais antigo.')
+
+            #aqui temos que separar os try pois se o servidor nao tiver um dos logs 
+            #o outro log automaticamente nao funciona
+            try:
+                # pass
+                successful_logins = c.run(f"journalctl -q -S today | grep -iE 'sshd:session'")
+                with open (sl_filename,"w") as sl:
+                    sl.write(successful_logins.stdout)
+
+            except Exception as e:
+                print('======')
+                print(e)
+                print('======')
+
+            try:
+                # pass
+                failed_logins = c.run(f"journalctl -q -S today | grep -iE 'sshd.+failed'")
+                with open (fl_filename,"w") as fl:
+                    fl.write(failed_logins.stdout)
+
+            except Exception as e:
+                print('======')
+                print(e)
+                print('======')
+
 
     except Exception as e:
-        print('nao foi possivel executar os comandos')
-        # print(e)   
+        pass
+        # print('nao foi possivel executar os comandos')
+        # print(f'sssss ----> {e}')   
 
 if __name__ == "__main__":
     main()
-    # time_stamp()
-    # pass
     # usage_message()
--- __PROJETOS_PRATICOS/01.automacao_python/backup_logs/tests1.py
+++ __PROJETOS_PRATICOS/01.automacao_python/backup_logs/tests1.py
@@ -1,19 +1,23 @@
-#!/usr/bin/env python
+# #!/usr/bin/env python
 
-import os, sys, pprint
-import yaml
+# import os, sys, pprint
+# import yaml
 
-with open('config.yaml','r') as f:
-    config = yaml.safe_load(f)
+# with open('config.yaml','r') as f:
+#     config = yaml.safe_load(f)
 
-# pprint.pprint(config)
-# pprint.pprint(os.listdir(config['destiny']))
-# # pprint.pprint(config['period'])
-# pprint.pprint(config['servers'][1]['name'])
-# pprint.pprint(config['servers'][1]['user'])
-# pprint.pprint(config['servers'][1]['server_ip'])
+# # pprint.pprint(config)
+# # pprint.pprint(os.listdir(config['destiny']))
+# # # pprint.pprint(config['period'])
+# # pprint.pprint(config['servers'][1]['name'])
+# # pprint.pprint(config['servers'][1]['user'])
+# # pprint.pprint(config['servers'][1]['server_ip'])
 
-for server in config['servers']:
-    pprint.pprint(server['name'])
-    pprint.pprint(server['user'])
-    pprint.pprint(server['server_ip'])
+# for server in config['servers']:
+#     pprint.pprint(server['name'])
+#     pprint.pprint(server['user'])
+#     pprint.pprint(server['server_ip'])
+
+
+a=['/tmp/reports/secondarylab/successful_logins/2024322_3-29---8_.txt', '/tmp/reports/secondarylab/successful_logins/2024322_3-30---24_.txt', '/tmp/reports/secondarylab/successful_logins/2024322_2-0---6_.txt', '/tmp/reports/secondarylab/successful_logins/2024322_1-59---35_.txt', '/tmp/reports/secondarylab/successful_logins/2024322_3-30---33_.txt', '/tmp/reports/secondarylab/successful_logins/2024322_3-32---2_.txt', '/tmp/reports/secondarylab/successful_logins/2024322_3-29---41_.txt']
+print(a.sort())
